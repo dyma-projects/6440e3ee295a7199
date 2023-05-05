1f24e0b8a0ff9e8e5b1e5ef100e135d1a7d587cf
Jonathan LEVEQUE <jleveque@scheiber.fr>
2023-05-05 11:33:55 +0200

    Chapitre6

--- src/app/exercice-1/exercice-1.component.ts
+++ src/app/exercice-1/exercice-1.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-exercice-1',
-  templateUrl: './exercice-1.component.html'
+  templateUrl: './exercice-1.component.html',
 })
 export class Exercice1Component implements OnInit {
-
   // Ce Hostlistener permet d'écouter un événement sur l'objet window global;
   // Peut-être que cela vous sera utile pour votre directive ;)
   // (Il vous faudra bien sûr le déplacer dans celle-ci)
@@ -14,10 +13,7 @@ export class Exercice1Component implements OnInit {
   //   console.log($event);
   // }
 
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
